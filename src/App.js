67e4303f01381fe397be476a742ec7d369b20cbd
ajyng <zxzimin@naver.com>
2020-12-02 16:55:07 +0900

    리액트 엘리먼트

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import 'App.css';
 
-//TODO: TodoList 구현
-//FIXME: FIXME 구현
-function TodoList() {
-  const title = '...';
-  const name = '...';
+class Counter1 extends React.Component {
+  state = {
+    value: this.props.initialVlaue,
+  };
+
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  };
+
+  render() {
+    const { value } = this.state; // 현재의 상탯값을 참조
+    return (
+      <div>
+        Counter 1: {value}
+        <Button onClick={this.onClick}>+1</Button>
+      </div>
+    );
+  }
+}
+
+class FruitComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          {this.props.fruits.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 function App() {
-  정;
+  const fruits = ['바나나', '사과', '딸기'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter1 initialVlaue={10} />
+      <FruitComponent fruits={fruits} />
     </div>
   );
 }
