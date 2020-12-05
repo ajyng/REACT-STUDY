5f480a0d413e6ddfc317f14815f703c9297f39b4
ajyng <zxzimin@naver.com>
2020-12-06 04:53:37 +0900

    React props/state 연습

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Button } from 'antd';
 import 'App.css';
 
+const actions = {
+  init(initialValue) {
+    return { value: initialValue };
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 };
+  },
+  decrement(prevState) {
+    return { value: prevState.value - 1 };
+  },
+};
+
+// 클래스형 Component
+//    => 컴포넌트의 내부 값들이 변경될 때마다 render() 함수가 호출된다.
 class Counter1 extends React.Component {
-  state = {
-    value: this.props.initialVlaue,
-  };
+  state = actions.init(this.props.initialValue);
 
   onClick = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
+    const value = this.state.value + 1;
+    // const { value } = this.state; // this.state에서 value 상탯값을 받아온다.
+    this.setState({ value }); // state명과 일치하기 때문. ex) {value:value}
+    // this.setState({ value: value + 1 });
   };
 
   render() {
@@ -17,33 +31,19 @@ class Counter1 extends React.Component {
     return (
       <div>
         Counter 1: {value}
-        <Button onClick={this.onClick}>+1</Button>
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>+1</Button>
       </div>
     );
   }
 }
 
-class FruitComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>좋아하는 과일</h1>
-        <ul>
-          {this.props.fruits.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
+// 함수형 Component
 function App() {
-  const fruits = ['바나나', '사과', '딸기'];
   return (
     <div>
-      <Counter1 initialVlaue={10} />
-      <FruitComponent fruits={fruits} />
+      <Counter1 initialValue={10} />{' '}
+      {/* 위에서 선언한 'Counter1'이라는 컴포넌트 활용 */}
     </div>
   );
 }
