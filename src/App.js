ffc0028e3714f270d2cea54d6a13794b3c60ce17
ajyng <zxzimin@naver.com>
2020-12-20 00:19:02 +0900

    속성값

--- src/App.js
+++ src/App.js
@@ -2,50 +2,62 @@ import React from 'react';
 import { Button } from 'antd';
 import 'App.css';
 
-const actions = {
-  init(initialValue) {
-    return { value: initialValue };
-  },
-  increment(prevState) {
-    return { value: prevState.value + 1 };
-  },
-  decrement(prevState) {
-    return { value: prevState.value - 1 };
-  },
-};
+class PostDetail extends React.Component {
+  state = {
+    postDetail: null,
+  }
 
-// 클래스형 Component
-//    => 컴포넌트의 내부 값들이 변경될 때마다 render() 함수가 호출된다.
-class Counter1 extends React.Component {
-  state = actions.init(this.props.initialValue);
+  componentDidMount() {
+    const {postId} = this.props;
+    this.requestPost(postId);
+  }
 
-  onClick = () => {
-    const value = this.state.value + 1;
-    // const { value } = this.state; // this.state에서 value 상탯값을 받아온다.
-    this.setState({ value }); // state명과 일치하기 때문. ex) {value:value}
-    // this.setState({ value: value + 1 });
-  };
+  componentDidUpdate(prevProps) {
+    const { postId } = this.props;
+    if ( postId != prevProps.postId ) {
+      this.requestPost(postId);
+    }
+  }
+
+  requestPost(postId) {
+    console.log(`request post #${postId}`);
+    this.setState({
+      postDetail: null
+    });
+
+    setTimeout( () => {
+      this.setState({
+        postDetail: `로딩된 post #${postId}`
+      })
+    }, 3000 );
+  }
 
   render() {
-    const { value } = this.state; // 현재의 상탯값을 참조
+    const { postId } = this.props;
+    const { postDetail } = this.state;
     return (
       <div>
-        Counter 1: {value}
-        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
-        <Button onClick={() => this.setState(actions.decrement)}>+1</Button>
+        포스팅 #{postId}
+        <hr/>
+        {!postDetail && "로딩 중 ..."}
+        {postDetail}
       </div>
-    );
+    )
   }
 }
 
-// 함수형 Component
-function App() {
-  return (
-    <div>
-      <Counter1 initialValue={10} />{' '}
-      {/* 위에서 선언한 'Counter1'이라는 컴포넌트 활용 */}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    postId: 10
+  }
+  render() {
+    return (
+      <div>
+        <PostDetail postId={this.state.postId} />
+        <Button onClick={ () => this.setState({ postId: 20}) }>postId 변경</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
